0fde488ea30babf453323822e17152c9909e1b73
dodabear42 <lhicks42@gmail.com>
2014-10-15 21:25:18 -0400

    jQuery added to aside
    toggle between knit and crochet thumbs

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-	$('#knitting-button').click(function() {
-		$('#knitting-thumbs').show();
+	$('#left-button').click(function() {
+		$('#right-button').css('background-color', '#c0c0c0');
+		$('#right-button').css('color', '#000');
+		$('#left-button').css('background-color', '#404040');
+		$('#left-button').css('color', '#fff');
 		$('#crochet-thumbs').hide();
+		$('#knitting-thumbs').slideDown(600);
 	});
-}
+	$('#right-button').click(function() {
+		$('#left-button').css('background-color', '#c0c0c0');
+		$('#left-button').css('color', '#000');
+		$('#right-button').css('background-color', '#404040');
+		$('#right-button').css('color', '#fff');
+		$('#crochet-thumbs').slideDown(600);
+		$('#knitting-thumbs').hide();
+	});
+});
