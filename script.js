1ede61ff574097ea86fc2e02682991c0f39dfe4a
dodabear42 <lhicks42@gmail.com>
2014-10-20 14:58:53 -0400

    content and styling updates
    add content and styling to finished projects, learn, homepage. adjusted
    some id and class names

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
 	$('#left-button').click(function() {
-		$('#right-button').css('background-color', '#c0c0c0');
-		$('#right-button').css('color', '#000');
-		$('#left-button').css('background-color', '#404040');
-		$('#left-button').css('color', '#fff');
+		$('#right-button').css('background-color', '#c0c0c0').css('color', '#000');
+		$('#left-button').css('background-color', '#404040').css('color', '#fff');
 		$('#crochet-thumbs').hide();
 		$('#knitting-thumbs').slideDown(600);
 	});
 	$('#right-button').click(function() {
-		$('#left-button').css('background-color', '#c0c0c0');
-		$('#left-button').css('color', '#000');
-		$('#right-button').css('background-color', '#404040');
-		$('#right-button').css('color', '#fff');
+		$('#left-button').css('background-color', '#c0c0c0').css('color', '#000');
+		$('#right-button').css('background-color', '#404040').css('color', '#fff');
 		$('#crochet-thumbs').slideDown(600);
 		$('#knitting-thumbs').hide();
+	});
+	$('.arrow-right').click(function() {
+		var currentSlide = $('.active-slide');
+		var newSlide = currentSlide.next();
+	});
+	$('.arrow-left').click(function() {
+		
 	});
 });
